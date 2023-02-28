0306fe2967d1ec3024127a38b94960a30c1e7163
OleksiiHolub <holub.oleksii@outlook.com>
2023-02-28 22:01:46 +0200

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Oleksii Holub on 28.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
